d35a530f66a16ac8acd8dd8633894989bc02665b
WillyC97 <wpj.chambers@gmail.com>
2024-03-11 07:31:06 +0000

    Add files via upload

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+function TrackHeader(props) {
+    return (
+        <div className='track-header'>
+            <div className="track-title">{props.title}</div>
+            <div className="track-buttons">
+               <div className="track-button left mute">M</div>
+               <div className="track-button solo">S</div>
+            </div>
+        </div>
+    );
+}
+
 function Track(props) {
     return (
         <div className='track'>
-        <div className="track-audio" style={{backgroundColor: props.backgroundColour}}/>
+            <TrackHeader title="Track 1"/>
+            <div className="track-audio" style={{backgroundColor: props.backgroundColour}}/>
         </div>
     );
 }
