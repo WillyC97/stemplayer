7cd38b3044f715cb865423ea339b1dba3687c8bc
WillyC97 <wpj.chambers@gmail.com>
2024-03-12 18:03:59 +0000

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,15 @@ import Track from './Track';
 
 function App() {
     return (
+      <div>
+        <div className='spacer-top' />
         <div className='d-flex flex-row'>
           <div className='flex-grow-1 flex-shrink-0'>
-            <Track backgroundColour="#ad1b1b"/>
-            <Track backgroundColour="#10e8cf"/>
+            <Track title="Track1" trackWidth="180px" backgroundColour="#ad1b1b"/>
+            <Track title="Track2" trackWidth="180px" backgroundColour="#10e8cf"/>
           </div>
         </div>
+      </div>
     );
 }
 
--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -4,9 +4,11 @@ function TrackHeader(props) {
     return (
         <div className='track-header'>
             <div className="track-title">{props.title}</div>
+           
             <div className="track-buttons">
                <div className="track-button left mute">M</div>
                <div className="track-button solo">S</div>
+               <input type="range" min="1" max="100" value="50" />
             </div>
         </div>
     );
@@ -15,8 +17,10 @@ function TrackHeader(props) {
 function Track(props) {
     return (
         <div className='track'>
-            <TrackHeader title="Track 1"/>
-            <div className="track-audio" style={{backgroundColor: props.backgroundColour}}/>
+            <TrackHeader title={props.title}/>
+            <div className="track-audio" style={{backgroundColor: props.backgroundColour}}>
+                <div className='track-seek-bar' style={{width: props.trackWidth }} />
+            </div>
         </div>
     );
 }
