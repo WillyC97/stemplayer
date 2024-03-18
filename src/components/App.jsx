6a3b43dc467b4be808b4330892ce61bba095a576
William Chambers <william.chambers@l-acoustics.com>
2024-03-18 17:01:08 +0000

    Add play/pause buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import Track from './Track';
+import React from "react";
+import Track from "./Track";
+import { useState } from "react";
 
 function App() {
-    return (
-      <div>
-        <div className='black-bar d-flex flex-row'>
-        </div>
-        <div className='d-flex flex-row'>
-          <div className='flex-grow-1 flex-shrink-0'>
-            <Track title="Track1" trackWidth="180px" backgroundColour="#ad1b1b"/>
-            <Track title="Track2" trackWidth="180px" backgroundColour="#10e8cf"/>
-          </div>
+  const [playing, setPlaying] = useState(false);
+
+  return (
+    <div>
+      <div className="black-bar d-flex flex-row">
+        <div className="btn">
+          {playing ? (
+            <div id="pause-button" onClick={() => setPlaying(false)} />
+          ) : (
+            <div id="play-button" onClick={() => setPlaying(true)} />
+          )}
         </div>
       </div>
-    );
+      <div className="d-flex flex-row">
+        <div className="flex-grow-1 flex-shrink-0">
+          <Track title="Track1" trackWidth="180px" backgroundColour="#ad1b1b" />
+          <Track title="Track2" trackWidth="180px" backgroundColour="#10e8cf" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
