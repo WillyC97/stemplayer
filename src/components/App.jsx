5a080c5a94df8377ea6f52fefc2bd57388d3c435
William Chambers <william.chambers@l-acoustics.com>
2024-04-08 17:22:52 +0100

    Switch to Web audio API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,168 +11,215 @@ import Running from "/Users/williamchambers/Developer/stemplayer/src/components/
 const TRACK_HEADER_WIDTH = 200;
 
 function App() {
-  const requestRef = useRef();
-  const previousTimeRef = useRef();
-
   const { height, width } = useWindowDimensions();
 
+  const requestRef = useRef();
+  const previousTimeRef = useRef();
+  const timingRef = useRef({ lastTimeStamp: 0.0, currentTime: 0.0 });
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audioContext, setAudioContext] = useState(null);
   const [seekBarWidth, setSeekbarWidth] = useState(0);
-  const [playing, setPlaying] = useState(false);
-  const [tracks, updateTrackState] = useState([
+  const [stems, updateStemState] = useState([
     {
       title: "Rave Digger",
       file: Rave,
       colour: "#ad1b1b",
-      howl: null,
+      buffer: null,
+      audioSource: null,
+      gainNode: null,
+      volume: 0.0,
       muted: false,
+      soloed: false,
     },
     {
       title: "80s Vibe",
       file: Vibe,
       colour: "#10e8cf",
-      howl: null,
+      buffer: null,
+      audioSource: null,
+      gainNode: null,
+      volume: 0.0,
       muted: false,
+      soloed: false,
     },
     {
       title: "Running Out",
       file: Running,
       colour: "#ad1b1b",
-      howl: null,
+      buffer: null,
+      audioSource: null,
+      gainNode: null,
+      volume: 0.0,
       muted: false,
+      soloed: false,
     },
   ]);
 
   useEffect(() => {
-    initialiseStems();
-    console.log("Initialise stems");
+    const ac = new (window.AudioContext || window.webkitAudioContext)();
+    setAudioContext(ac);
+
+    Promise.all(
+      stems.map((stem) =>
+        fetch(stem.file)
+          .then((response) => response.arrayBuffer())
+          .then((buffer) => {
+            return { ...stem, buffer: buffer, uuid: crypto.randomUUID() };
+          })
+      )
+    ).then((initialisedStems) => {
+      updateStemState(initialisedStems);
+    });
   }, []);
 
-  function initialiseStems() {
-    const updatedTracks = tracks.map((track) => {
-      return {
-        ...track,
-        howl: new Howl({
-          src: [track.file],
-          html5: true,
-        }),
-        uuid: crypto.randomUUID(),
-        soloed: false,
-      };
+  function playAudio() {
+    const newTracks = stems.map(async (track) => {
+      let source = null;
+      let gain = null;
+      if (true) {
+        source = audioContext.createBufferSource();
+        gain = audioContext.createGain();
+        if (!(track.buffer instanceof AudioBuffer)) {
+          track.buffer = await audioContext.decodeAudioData(track.buffer);
+        }
+        source.buffer = track.buffer;
+        source.connect(gain);
+        gain.connect(audioContext.destination);
+      }
+      return { ...track, audioSource: source, gainNode: gain };
     });
 
-    updateTrackState(updatedTracks);
-  }
-
-  function updateMuteState(trackUUID) {
-    const audioSource = tracks.find(data => data.uuid === trackUUID);
-    if (audioSource) {
-      console.log("updating mute state");
-      updateTrackState(tracks.map((data) => data.uuid === trackUUID ? { ...data, muted: !data.muted } : data));
-      toggleTrackMute(trackUUID, !audioSource.muted);
-    } else {
-      console.error(`No audio source found with id ${trackUUID}`);
-    }
-  }
-
-  function updateSoloState(trackUUID) {
-    const updatedTracks = [...tracks];
-
-    const trackIndex = updatedTracks.findIndex(
-      (track) => track.uuid === trackUUID
-    );
-
-    if (trackIndex === -1) return;
-
-    updatedTracks[trackIndex].soloed = !updatedTracks[trackIndex].soloed;
-
-    updateTrackState(updatedTracks);
-
-    toggleTrackSolo();
-  }
-
-  function toggleTrackMute(trackUUID, muteState) {
-    const audioSource = tracks.find(data => data.uuid === trackUUID);
-    audioSource.howl.mute(audioSource.muted);
-
-    console.log(tracks);
-  }
-
-  function toggleTrackSolo() {
-
-    if (isSoloActive()) {
-      tracks.forEach(track => {
-        track.howl.mute(!track.soloed);
+    Promise.all(newTracks).then((updatedStems) => {
+      updateStemState(updatedStems);
+      setIsPlaying(true);
+      updatedStems.forEach((stem) => {
+        stem.audioSource.start(0.02, timingRef.current.currentTime);
       });
-    } else {
-      tracks.forEach((track) => {
-        track.howl.mute(track.muted);
-      });
-    }
+      timingRef.current.lastTimeStamp = audioContext.currentTime;
+      requestRef.current = requestAnimationFrame(clockTick);
+    });
   }
 
-  function isSoloActive() {
-    return tracks.some((track) => track.soloed);
-  }
-
-  function onSeekBarClick(e) {
-    const percentage =
-      (e.clientX - TRACK_HEADER_WIDTH) / (width - TRACK_HEADER_WIDTH);
-
-    tracks.forEach((track) => {
-      track.howl.seek(percentage * track.howl.duration());
+  function pauseAudio() {
+    stems.forEach((stem) => {
+      stem.audioSource.stop();
     });
 
-    updateSeekBar();
-  }
-
-  function onPlayPause() {
-    playing ? pause() : play();
-  }
-
-  function play() {
-    setPlaying(true);
-    console.log("playing");
-
-    tracks.forEach((track) => {
-      track.howl.play();
-    });
-    requestRef.current = requestAnimationFrame(animate);
-  }
-
-  function pause() {
-    setPlaying(false);
-    console.log("paused");
-
-    tracks.forEach((track) => {
-      track.howl.pause();
-    });
+    setIsPlaying(false);
     cancelAnimationFrame(requestRef.current);
   }
 
-  function updateSeekBar() {
-    var masterHowl = tracks[0].howl;
-    if (masterHowl) {
-      setSeekbarWidth(
-        (masterHowl.seek() / masterHowl.duration()) *
-          (width - TRACK_HEADER_WIDTH)
-      );
+  function updateMuteState(trackUUID) {
+    // const audioSource = tracks.find(data => data.uuid === trackUUID);
+    // if (audioSource) {
+    //   console.log("updating mute state");
+    //   updateTrackState(tracks.map((data) => data.uuid === trackUUID ? { ...data, muted: !data.muted } : data));
+    //   toggleTrackMute(trackUUID, !audioSource.muted);
+    // } else {
+    //   console.error(`No audio source found with id ${trackUUID}`);
+    // }
+  }
+
+  function updateSoloState(trackUUID) {
+    // const updatedTracks = [...tracks];
+    // const trackIndex = updatedTracks.findIndex(
+    //   (track) => track.uuid === trackUUID
+    // );
+    // if (trackIndex === -1) return;
+    // updatedTracks[trackIndex].soloed = !updatedTracks[trackIndex].soloed;
+    // updateTrackState(updatedTracks);
+    // toggleTrackSolo();
+  }
+
+  function toggleTrackMute(trackUUID, muteState) {
+    const audioSource = stems.find((data) => data.uuid === trackUUID);
+    if (audioSource.muted) {
     }
   }
 
-  const animate = (time) => {
+  function toggleTrackSolo() {
+    // if (isSoloActive()) {
+    //   tracks.forEach(track => {
+    //     track.howl.mute(!track.soloed);
+    //   });
+    // } else {
+    //   tracks.forEach((track) => {
+    //     track.howl.mute(track.muted);
+    //   });
+    // }
+  }
+
+  function isSoloActive() {
+    // return tracks.some((track) => track.soloed);
+  }
+
+  function onSeekBarClick(e) {
+    // const percentage =
+    //   (e.clientX - TRACK_HEADER_WIDTH) / (width - TRACK_HEADER_WIDTH);
+    // tracks.forEach((track) => {
+    //   track.howl.seek(percentage * track.howl.duration());
+    // });
+    // updateSeekBar();
+  }
+
+  function onPlayPause() {
+    isPlaying ? pauseAudio() : playAudio();
+  }
+
+  // function play() {
+  //   setPlaying(true);
+  //   console.log("playing");
+
+  //   tracks.forEach((track) => {
+  //     track.howl.play();
+  //   });
+  //   requestRef.current = requestAnimationFrame(animate);
+  // }
+
+  // function pause() {
+  //   setPlaying(false);
+  //   console.log("paused");
+
+  //   tracks.forEach((track) => {
+  //     track.howl.pause();
+  //   });
+  //   cancelAnimationFrame(requestRef.current);
+  // }
+
+  function updateSeekBar() {
+    // var masterHowl = tracks[0].howl;
+    // if (masterHowl) {
+    //   setSeekbarWidth(
+    //     (masterHowl.seek() / masterHowl.duration()) *
+    //       (width - TRACK_HEADER_WIDTH)
+    //   );
+    // }
+  }
+
+  // Clock/timing
+  const clockTick = (time) => {
+    const timeChange =
+      audioContext.currentTime - timingRef.current.lastTimeStamp;
+
+    timingRef.current = {
+      lastTimeStamp: audioContext.currentTime,
+      currentTime: timingRef.current.currentTime + timeChange,
+    };
+
     if (previousTimeRef.current != undefined) {
-      updateSeekBar();
+      // updateSeekBar();
     }
 
     previousTimeRef.current = time;
-    requestRef.current = requestAnimationFrame(animate);
+    requestRef.current = requestAnimationFrame(clockTick);
   };
 
   return (
     <div>
       <div className="black-bar d-flex flex-row">
         <div className="btn">
-          {playing ? (
+          {isPlaying ? (
             <div id="pause-button" onClick={() => onPlayPause()} />
           ) : (
             <div id="play-button" onClick={() => onPlayPause()} />
@@ -181,7 +228,7 @@ function App() {
       </div>
       <div className="d-flex flex-row">
         <div className="flex-grow-1 flex-shrink-0">
-          {tracks.map((track) => (
+          {stems.map((track) => (
             <Track
               track={track}
               title={track.title}
