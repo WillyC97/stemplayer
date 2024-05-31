52b792eb11b920727e0046b981e41bb8114a44ab
William Chambers <williamchambers@Williams-MacBook-Pro.local>
2024-05-31 23:35:53 +0100

    "Fix" time string not being set

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,6 +1,6 @@
 // Seconds to Minutes
 export function secondsToMinutes(seconds) {
-    if (!seconds) { return ''; }
+    if (!seconds) { return '0:00'; }
     var rounded_seconds = Math.round(seconds);
     var mins = Math.floor(rounded_seconds / 60.0);
     var secs = rounded_seconds % 60;
